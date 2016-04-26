e42ad4eac9682635cf3276e1e2a50579164402b9
chmzhang <chanmionezhang@gmail.com>
2016-04-26 17:58:30 +0800

    modify json url

--- show.php
+++ show.php
@@ -1,14 +1,14 @@
 <?php
 header("Content-Type:text/html; charset=utf-8");
-	switch($_POST['tab']){
+	switch($_GET['tab']){
 	case 1:
-		$json=file_get_contents('http://github.com/chmzhang/library/info1.json');
+		$json=file_get_contents('info1.json');
 		break;
 	case 2:
-		$json=file_get_contents('http://github.com/chmzhang/library/info2.json');
+		$json=file_get_contents('/library/info2.json');
 		break;
 	case 3:
-    	$json=file_get_contents('http://github.com/chmzhang/library/info3.json');
+    	$json=file_get_contents('http://chmzhang.github.io/library/info3.json');
     	break;
     default:
     	break;
