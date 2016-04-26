d199757d1ffad5315dc4c2e99b38565401d543d0
chmzhang <chanmionezhang@gmail.com>
2016-04-26 20:31:19 +0800

    test login

--- login.php
+++ login.php
@@ -1,5 +1,6 @@
 <?php
-
+header('Access-Control-Allow-Origin:http://chmzhang.github.io/library/');
+header('Access-Control-Allow-Methods:POST,GET');
     if(($_POST['login_user']=='admin') && ($_POST['login_pass']=='password')){
         echo 'true';
     }else{
